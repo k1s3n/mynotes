a5f1ccb6e9fc82df88a5ba30eb9026039011f646
Christoffer <Christoffer@Rehnmark.se>
2024-09-24 02:25:57 +0200

    added cors

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');  // Importera cors
 const connectDB = require('./config/db');
 const postRoutes = require('./routes/postRoutes');
 const userRoutes = require('./routes/userRoutes');
@@ -10,6 +11,11 @@ connectDB();
 
 const app = express();
 app.use(express.json());
+
+// Middleware för att hantera CORS
+app.use(cors({
+    origin: 'http://localhost:3000'  // Tillåt förfrågningar från React-appen
+  }));
 
 app.use('/api/posts', postRoutes);
 app.use('/api/users', userRoutes);
